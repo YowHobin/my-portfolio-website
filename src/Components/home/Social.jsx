0da1983747bbe03ffcf5a40a8af621ccf792989b
YowHobin <lruarellano@rtu.edu.ph>
2023-07-18 19:43:57 +0800

    portfolio about

---
+++ src/Components/home/Social.jsx
@@ -0,0 +1,32 @@
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://m.me/LenardRoyArellano"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-facebook-messenger-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/YowHobin"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/lenard-roy-arellano-485845266/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
